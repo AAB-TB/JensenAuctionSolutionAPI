21d2df2a72fe680ac7e8028f121c87a3c8ea8848
Alvin Alamgir berglund <alvinalamgirberglund@gmail.com>
2024-01-12 15:08:03 +0100

    Grunder

---
+++ JensenAuctionSolutionDomain/Models/Auction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JensenAuctionSolutionDomain.Models
+{
+    public class Auction
+    {
+        public int AuctionId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        // Foreign key for Users table
+        public int UserId { get; set; }
+
+        // Navigation property for User
+        public User User { get; set; }
+
+        // Navigation property for Bids
+        public ICollection<Bid> Bids { get; set; }
+    }
+}
---
+++ JensenAuctionSolutionDomain/Models/Bid.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JensenAuctionSolutionDomain.Models
+{
+    public class Bid
+    {
+        public int BidId { get; set; }
+        public decimal Price { get; set; }
+
+        
+        public int AuctionId { get; set; }
+
+        
+        public Auction Auction { get; set; }
+
+        
+        public int UserId { get; set; }
+
+        
+        public User User { get; set; }
+    }
+}
