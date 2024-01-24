2fb45ffd74811139f6a0562888983c81c3dd34e3
Alvin Alamgir berglund <alvinalamgirberglund@gmail.com>
2024-01-24 10:15:09 +0100

    AuctionInterfaces

--- JensenAuctionSolutionCommonInterfaces/Interfaces/ICommonAuctionService.cs
+++ JensenAuctionSolutionCommonInterfaces/Interfaces/ICommonAuctionService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using JensenAuctionSolutionCommonInterfaces.DTOs;
+using JensenAuctionSolutionDomain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,17 @@ namespace JensenAuctionSolutionCommonInterfaces.Interfaces
 {
     public interface ICommonAuctionService
     {
+        Task<Auction> CreateAuctionAsync(int userid, AuctionDto auctionDto);
+
+        Task<AuctionDetailsInfoDto> UpdateAuctionAsync(int UserId, int auctionId, AuctionDto auctionDto);
+
+        Task<bool> RemoveAuctionAsync(int auctionId);
+
+        Task<IEnumerable<AuctionInfoDto>> GetAllAuctionsAsync();
+
+        Task<AuctionDetailsInfoDto> GetAuctionDetailsAsync(int auctionId);
+
+        Task<IEnumerable<AuctionDetailsInfoDto>> SearchAuctionsAsync(string searchQuery);
+
     }
 }
